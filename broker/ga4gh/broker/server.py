252da654452fba8772c61a5294d46698ed92c697
akash2237778 <akash2237778@gmail.com>
2021-06-17 00:18:26 +0530

    Everything working on mock data

--- broker/ga4gh/broker/server.py
+++ broker/ga4gh/broker/server.py
@@ -1,5 +1,7 @@
 """Controllers for broker endpoints"""
 
+import json
+
 from flask import (current_app, make_response)
 
 from foca.utils.logging import log_traffic
@@ -14,56 +16,59 @@ from broker.errors.exceptions import (
 
 @log_traffic
 def getRepositories():
-    return [{'projectId':'project_12',  'type':'production', 'url':'https://goo.gl', 'dockerfileLocation':'./docker', 'containerRegistryUrl':'gcr.io/my-project/busybox', 'containerRegistryToken':'XXXXXXXXXXXXXXX' }]
-
+    return json.loads('[{"url":"https", "repository_id": "repo_123"}]')
 @log_traffic
 def postRepositories():
-    return "Template function: postProjects"
+    return json.loads('{"access_token": "xxxxxxxxxxxxxx","id": "repository_123" }')
 
-@log_traffic
-def postProjects():
-    return "Template function: postProjects"
+# @log_traffic
+# def postProjects():
+#     return json.loads('')
 
 @log_traffic
 def getRepository(id: str):
-    return "Template function: postBuild"
+    return json.loads('{"url":"https://localhost:8080/repositories", "repository_id": "repo_123"}')
 
 @log_traffic
 def putRepositories(id: str):
-    return "Template function: getBuilds"
+    return json.loads('{  "access_token": "xxxxxxxxxxxxxx",  "id": "repository_123"}')
 
 @log_traffic
 def deleteRepository(id: str):
-    return "Template function: deleteProject"
+    return json.loads('{"message": "Repository deleted successfully"}')
 
 @log_traffic
 def postBuild(id: str):
-    return "Template function: modifyBuild"
+    return json.loads('{"id": "build_123"}')
 
 @log_traffic
 def getBuilds(id: str):
-    return "Template function: getBuildInfo"
+    return json.loads('[   {    "images": [        {            "name": "akash7778/broker:0.0.1",            "location": "./Dockerfile"        },        {            "name": "akash7778/broker:0.0.1",            "location": "./Dockerfile"        }    ],    "head_commit": {        "branch": "development",        "commit_sha": "930fd5"    },    "status": "UNKNOWN",    "started_at" : "2021-06-11T17:32:28Z",    "finished_at": "2021-06-11T17:32:28Z"},    {    "images": [        {            "name": "akash7778/broker:0.0.1",            "location": "./Dockerfile"        },        {            "name": "akash7778/broker:0.0.1",            "location": "./Dockerfile"        }    ],    "head_commit": {        "branch": "development",        "commit_sha": "930fd5"    },    "status": "UNKNOWN",    "started_at" : "2021-06-11T17:32:28Z",    "finished_at": "2021-06-11T17:32:28Z"} ]')
 
 @log_traffic
 def getBuildInfo(id: str, build_id: str):
-    return "Template function: deleteBuild"
+    return json.loads('    {    "images": [        {            "name": "akash7778/broker:0.0.1",            "location": "./Dockerfile"        },        {            "name": "akash7778/broker:0.0.1",            "location": "./Dockerfile"        }    ],    "head_commit": {        "branch": "development",        "commit_sha": "930fd5"    },    "status": "UNKNOWN",    "started_at" : "2021-06-11T17:32:28Z",    "finished_at": "2021-06-11T17:32:28Z"}')
+
+
+
+   
 
 @log_traffic
 def postSubscription():
-    return "Template function: postService"
+    return json.loads('{ "subscription_id": "subscription-xyz"}')
 
 @log_traffic
 def getSubscriptions():
-    return "Template function: getServices"
+    return json.loads('[  { "subscription_id": "subscription-xyz"  }]')
 
 @log_traffic
 def modifySubscription(subscription_id: str):
-    return "Template function: modifyService"
+    return json.loads(' { "subscription_id": "subscription-xyz"  }')
 
 @log_traffic
 def getSubscriptionInfo(subscription_id: str):
-    return "Template function: getServiceInfo"
+    return json.loads('{  "callback_url": "https://ec2-54-203-145-132.compute-1.amazonaws.com/update",  "repository_id": "respository123",  "build_id": "build_123",  "build_type": "production",  "state": "Active",  "updated_at": "2021-06-11T17:32:28+00:00"}')
 
 @log_traffic
 def deleteSubscription(subscription_id: str):
-    return "Template function: deleteService"
+    return json.loads('{ "subscription_id": "subscription-xyz"  }')
