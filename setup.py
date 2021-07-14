d40e547165c7a81ec9ac0778f88796b59e455511
akash2237778 <akash2237778@gmail.com>
2021-07-14 14:15:11 +0530

    WIP: line wraps

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ setup(
     # version=__version__,
     author='ELIXIR Cloud & AAI',
     author_email='akash2237778@gmail.com',
-    description='Broker is a decoupled, publish-subscribe-based \
-                     continuous integration (CI) and continuous \
-                     delivery (CD) microservice that allows \
-                     available updates. ',
+    description='Broker is a decoupled, publish-subscribe-based continuous '
+                'integration (CI) and continuous delivery (CD) microservice '
+                'that allows developers to notify deployments of available '
+                'updates, which can then autonomously decide what to do with '
+                'them. ',
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='Apache License 2.0',
