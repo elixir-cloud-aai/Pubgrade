3535f2bba126176a7b88ac0bc5ce0ee92ec1d4e9
akash2237778 <akash2237778@gmail.com>
2022-03-01 23:08:20 +0530

    feat: User management with admin user

--- pubgrade/app.py
+++ pubgrade/app.py
@@ -13,24 +13,30 @@ def create_admin_user(app):
     Function is used to create admin user.
     """
     with app.app.app_context():
-        uid = current_app.config['FOCA'].endpoints['subscriptions'][
-            'admin_user']['uid']
-        name = current_app.config['FOCA'].endpoints['subscriptions'][
-            'admin_user']['name']
-        user_access_token = current_app.config[
-            'FOCA'].endpoints['subscriptions'][
-            'admin_user']['user_access_token']
-        db_collection = (
-            current_app.config['FOCA'].db.dbs['pubgradeStore'].
-            collections['users'].client
-        )
-        data = db_collection.find({"uid": uid})
-        if data.count() == 0:
-            db_collection.insert_one({
-                'uid': uid,
-                'name': name,
-                'user_access_token': user_access_token
-            })
+
+        # Adding admin users from `config.yaml`.
+        admin_users = current_app.config["FOCA"].endpoints["subscriptions"][
+            "admin_users"
+        ]
+        for user in admin_users:
+            uid = user["uid"]
+            name = user["name"]
+            user_access_token = user["user_access_token"]
+            db_collection = (
+                current_app.config["FOCA"]
+                .db.dbs["pubgradeStore"]
+                .collections["admin_users"]
+                .client
+            )
+            data = db_collection.find({"uid": uid})
+            if data.count() == 0:
+                db_collection.insert_one(
+                    {
+                        "uid": uid,
+                        "name": name,
+                        "user_access_token": user_access_token,
+                    }
+                )
 
 
 def main():
@@ -39,5 +45,5 @@ def main():
     app.run(port=app.port)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- tests/pubgrade/endpoints/test_repositories.py
+++ tests/pubgrade/endpoints/test_repositories.py
@@ -12,19 +12,15 @@ from pymongo.errors import DuplicateKeyError
 from werkzeug.exceptions import Unauthorized, InternalServerError
 
 from pubgrade.errors.exceptions import URLNotFound, RepositoryNotFound
-from pubgrade.pubgrade.endpoints.repositories import (
+from pubgrade.modules.endpoints.repositories import (
     register_repository,
     get_repositories,
     generate_id,
     get_repository,
     modify_repository_info,
-    delete_repository
+    delete_repository,
 )
-from tests.mock_data import (
-    MONGO_CONFIG,
-    ENDPOINT_CONFIG,
-    MOCK_REPOSITORIES
-)
+from tests.mock_data import MONGO_CONFIG, ENDPOINT_CONFIG, MOCK_REPOSITORIES
 
 
 class TestRepository:
@@ -33,63 +29,61 @@ class TestRepository:
     repository_url = "https://github.com/elixir-cloud-aai/drs-filer"
 
     def setup(self):
-        self.app.config['FOCA'] = \
-            Config(db=MongoConfig(**MONGO_CONFIG), endpoints=ENDPOINT_CONFIG)
-        self.app.config['FOCA'].db.dbs['pubgradeStore']. \
-            collections['repositories'].client = mongomock.MongoClient(
-        ).db.collection
+        self.app.config["FOCA"] = Config(
+            db=MongoConfig(**MONGO_CONFIG), endpoints=ENDPOINT_CONFIG
+        )
+        self.app.config["FOCA"].db.dbs["pubgradeStore"].collections[
+            "repositories"
+        ].client = mongomock.MongoClient().db.collection
         for repository in MOCK_REPOSITORIES:
-            self.app.config['FOCA'].db.dbs['pubgradeStore']. \
-                collections['repositories'].client.insert_one(
-                repository).inserted_id
+            self.app.config["FOCA"].db.dbs["pubgradeStore"].collections[
+                "repositories"
+            ].client.insert_one(repository)
 
     def test_register_repository(self):
         self.setup()
-        data = {
-            "url": self.repository_url
-        }
+        data = {"url": self.repository_url}
         with self.app.app_context():
             res = register_repository(data=data)
-            assert 'id' in res
-            assert 'access_token' in res
+            assert "id" in res
+            assert "access_token" in res
             assert isinstance(res, dict)
 
     def test_register_repository_url_not_found(self):
         self.setup()
-        data = {
-        }
+        data = {}
         with self.app.app_context():
             with pytest.raises(URLNotFound):
                 res = register_repository(data=data)
-                assert 'id' in res
-                assert 'access_token' in res
+                assert "id" in res
+                assert "access_token" in res
                 assert isinstance(res, dict)
 
     def test_register_repository_duplicate_key_error(self):
         app = Flask(__name__)
-        app.config['FOCA'] = \
-            Config(
-                db=MongoConfig(**MONGO_CONFIG),
-                endpoints=ENDPOINT_CONFIG,
-            )
-        mock_resp = MagicMock(side_effect=DuplicateKeyError(''))
-        app.config['FOCA'].db.dbs['pubgradeStore'].collections[
-            'repositories'].client = MagicMock()
-        app.config['FOCA'].db.dbs['pubgradeStore'].collections[
-            'repositories'].client.insert_one = mock_resp
-        request_data = {
-            "url": self.repository_url
-        }
+        app.config["FOCA"] = Config(
+            db=MongoConfig(**MONGO_CONFIG),
+            endpoints=ENDPOINT_CONFIG,
+        )
+        mock_resp = MagicMock(side_effect=DuplicateKeyError(""))
+        app.config["FOCA"].db.dbs["pubgradeStore"].collections[
+            "repositories"
+        ].client = MagicMock()
+        app.config["FOCA"].db.dbs["pubgradeStore"].collections[
+            "repositories"
+        ].client.insert_one = mock_resp
+        request_data = {"url": self.repository_url}
         with app.app_context():
             with pytest.raises(InternalServerError):
                 register_repository(data=request_data)
 
     def test_get_repositories(self):
-        self.app.config['FOCA'] = \
-            Config(db=MongoConfig(**MONGO_CONFIG), endpoints=ENDPOINT_CONFIG)
-        self.app.config['FOCA'].db.dbs['pubgradeStore']. \
-            collections['repositories'].client = mongomock.MongoClient(
-        ).db.collection
+        self.app.config["FOCA"] = Config(
+            db=MongoConfig(**MONGO_CONFIG), endpoints=ENDPOINT_CONFIG
+        )
+        self.app.config["FOCA"].db.dbs["pubgradeStore"].collections[
+            "repositories"
+        ].client = mongomock.MongoClient().db.collection
         with self.app.app_context():
             res = get_repositories()
             assert res == []
@@ -97,10 +91,10 @@ class TestRepository:
         with self.app.app_context():
             res = get_repositories()
             assert isinstance(res, list)
-            assert MOCK_REPOSITORIES[1]['id'] == res[1]['id']
+            assert MOCK_REPOSITORIES[1]["id"] == res[1]["id"]
 
     def test_generate_id(self):
-        string.digits       # to avoid lint error
+        string.digits  # to avoid lint error
         for i in range(100):
             charset = "string.ascii_lowercase + string.digits +  '.' +  '-'"
             charset = eval(charset)
@@ -108,78 +102,74 @@ class TestRepository:
             res = generate_id(charset, length)
             assert length == len(res)
             assert 1 in [c in res for c in charset]
-            assert '_' not in res or res[0] != '.' or res[0] != '-'
+            assert "_" not in res or res[0] != "." or res[0] != "-"
 
     def test_get_repository(self):
         self.setup()
         with self.app.app_context():
-            res = get_repository(MOCK_REPOSITORIES[1]['id'])
-            assert 'subscription_list' not in res
-            assert 'access_token' not in res
-            assert 'build_list' in res and MOCK_REPOSITORIES[1]['build_list'] \
-                   == res['build_list']
-            assert 'id' in res and MOCK_REPOSITORIES[1]['id'] \
-                   == res['id']
-            assert 'url' in res and MOCK_REPOSITORIES[1]['url'] \
-                   == res['url']
+            res = get_repository(MOCK_REPOSITORIES[1]["id"])
+            assert "subscription_list" not in res
+            assert "access_token" not in res
+            assert (
+                "build_list" in res
+                and MOCK_REPOSITORIES[1]["build_list"] == res["build_list"]
+            )
+            assert "id" in res and MOCK_REPOSITORIES[1]["id"] == res["id"]
+            assert "url" in res and MOCK_REPOSITORIES[1]["url"] == res["url"]
 
     def test_get_repository_repository_not_found(self):
         self.setup()
         with self.app.app_context():
             with pytest.raises(RepositoryNotFound):
-                get_repository('abcd')
+                get_repository("abcd")
 
     def test_modify_repository_info(self):
         self.setup()
-        data = {
-            "url": self.repository_url
-        }
+        data = {"url": self.repository_url}
         with self.app.app_context():
-            res = modify_repository_info(MOCK_REPOSITORIES[1]['id'],
-                                         MOCK_REPOSITORIES[1]['access_token'],
-                                         data)
-            assert 'access_token' in res and 'id' in res
-            assert res['id'] == MOCK_REPOSITORIES[1]['id']
+            res = modify_repository_info(
+                MOCK_REPOSITORIES[1]["id"],
+                MOCK_REPOSITORIES[1]["access_token"],
+                data,
+            )
+            assert "access_token" in res and "id" in res
+            assert res["id"] == MOCK_REPOSITORIES[1]["id"]
 
     def test_modify_repository_info_repository_not_found(self):
         self.setup()
-        data = {
-            "url": self.repository_url
-        }
+        data = {"url": self.repository_url}
         with self.app.app_context():
             with pytest.raises(RepositoryNotFound):
-                modify_repository_info('abcd',
-                                       MOCK_REPOSITORIES[1]['access_token'],
-                                       data)
+                modify_repository_info(
+                    "abcd", MOCK_REPOSITORIES[1]["access_token"], data
+                )
 
     def test_modify_repository_info_unauthorized(self):
         self.setup()
-        data = {
-            "url": self.repository_url
-        }
+        data = {"url": self.repository_url}
         with self.app.app_context():
             with pytest.raises(Unauthorized):
-                modify_repository_info(MOCK_REPOSITORIES[1]['id'],
-                                       'access_token',
-                                       data)
+                modify_repository_info(
+                    MOCK_REPOSITORIES[1]["id"], "access_token", data
+                )
 
     def test_delete_repository(self):
         self.setup()
         with self.app.app_context():
-            res = delete_repository(MOCK_REPOSITORIES[1]['id'],
-                                    MOCK_REPOSITORIES[1]['access_token'])
+            res = delete_repository(
+                MOCK_REPOSITORIES[1]["id"],
+                MOCK_REPOSITORIES[1]["access_token"],
+            )
             assert res == 1
 
     def test_delete_repository_repository_not_found(self):
         self.setup()
         with self.app.app_context():
             with pytest.raises(RepositoryNotFound):
-                delete_repository('abcd',
-                                  MOCK_REPOSITORIES[1]['access_token'])
+                delete_repository("abcd", MOCK_REPOSITORIES[1]["access_token"])
 
     def test_delete_repository_unauthorized(self):
         self.setup()
         with self.app.app_context():
             with pytest.raises(Unauthorized):
-                delete_repository(MOCK_REPOSITORIES[1]['id'],
-                                  'access_token')
+                delete_repository(MOCK_REPOSITORIES[1]["id"], "access_token")
